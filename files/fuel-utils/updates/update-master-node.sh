2e7fa73441354918fd5f4ceda0bbcdbe7c97872b
Mykyta Karpin <mkarpin@mirantis.com>
2017-02-20 18:36:38 +0200

    Test commit for troubleshooting CI

--- files/fuel-utils/updates/update-master-node.sh
+++ files/fuel-utils/updates/update-master-node.sh
@@ -25,7 +25,7 @@ systemctl daemon-reload
 
 echo '<STAGE>: Puppet...'
 # re-apply puppet master node configuration
-bash -x /etc/puppet/modules/fuel/examples/deploy.sh
+bash -x /etc/puppet/modules/fuel/examples/deploy2.sh
 
 
 echo '<STAGE>: Services...'
